dca30d17cc58e75c9f75e5f6ec3cd3ffb439f7ff
Harikarandev <achumrh@gmail.com>
2023-08-07 22:54:58 +0530

    changes for comments_api

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,13 @@
 //CORE_REFERENCE_IMPORTS
+//CORE_REFERENCE_IMPORT-comments-SSD_SERVICE_ID_sd_2vrYmZDBSgx8xHIt
+import { comments as SSD_SERVICE_ID_sd_2vrYmZDBSgx8xHIt } from './sd-services/comments/comments';
 //CORE_REFERENCE_IMPORT-login-SSD_SERVICE_ID_sd_2NWhw0N4zVsBdyXO
 import { login as SSD_SERVICE_ID_sd_2NWhw0N4zVsBdyXO } from './sd-services/login/login';
 
 export const UserRoutes = [
     //CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY
+//CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY-comments-SSD_SERVICE_ID_sd_2vrYmZDBSgx8xHIt
+SSD_SERVICE_ID_sd_2vrYmZDBSgx8xHIt,
 //CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY-login-SSD_SERVICE_ID_sd_2NWhw0N4zVsBdyXO
 SSD_SERVICE_ID_sd_2NWhw0N4zVsBdyXO,
 ];
---
+++ server/src/sd-services/comments/comments.ts
@@ -0,0 +1,124 @@
+let instance = null;
+//CORE_REFERENCE_IMPORTS
+//append_imports_start
+
+import { StatusCodes as httpStatusCodes } from 'http-status-codes'; //_splitter_
+import * as cookieParser from 'cookie-parser'; //_splitter_
+import { Readable } from 'stream'; //_splitter_
+import { setInterval } from 'timers'; //_splitter_
+import { Issuer, custom } from 'openid-client'; //_splitter_
+import * as crypto from 'crypto'; //_splitter_
+import * as url from 'url'; //_splitter_
+import { SDBaseService } from '../../services/SDBaseService'; //_splitter_
+import { Middleware } from '../../middleware/Middleware'; //_splitter_
+import * as settings from '../../config/config'; //_splitter_
+import log from '../../utils/Logger'; //_splitter_
+import { TracerService } from '../../services/TracerService'; //_splitter_
+//append_imports_end
+export class comments {
+  private sdService = new SDBaseService();
+  private tracerService = new TracerService();
+  private app;
+  private serviceBasePath: string;
+  private generatedMiddlewares: Object;
+  private serviceName: string;
+
+  private globalTimers: any;
+  private constructor(
+    app,
+    generatedeMiddlewares,
+    routeCall,
+    middlewareCall,
+    globalTimers
+  ) {
+    this.serviceName = 'comments';
+    this.app = app;
+    this.serviceBasePath = this.app.settings.base;
+    this.generatedMiddlewares = generatedeMiddlewares;
+    this.globalTimers = globalTimers;
+  }
+
+  static getInstance(
+    app?,
+    generatedeMiddlewares?,
+    routeCall?,
+    middlewareCall?,
+    globalTimers?
+  ) {
+    if (!instance) {
+      instance = new comments(
+        app,
+        generatedeMiddlewares,
+        routeCall,
+        middlewareCall,
+        globalTimers
+      );
+    }
+    instance.mountCalls(routeCall, middlewareCall);
+    return instance;
+  }
+
+  private mountCalls(routeCall, middlewareCall) {
+    if (routeCall) {
+      this.mountAllPaths();
+      this.mountAllListeners();
+    }
+    if (middlewareCall) {
+      this.generatedMiddlewares[this.serviceName] = {};
+      this.mountAllMiddlewares();
+      this.mountTimers();
+    }
+  }
+
+  async mountAllListeners() {
+    try {
+      //append_listeners
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async mountTimers() {
+    try {
+      //appendnew_flow_comments_TimerStart
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  private mountAllMiddlewares() {
+    log.debug('mounting all middlewares for service :: comments');
+
+    //appendnew_flow_comments_MiddlewareStart
+  }
+  private mountAllPaths() {
+    log.debug('mounting all paths for service :: comments');
+    //appendnew_flow_comments_HttpIn
+  }
+  //   service flows_comments
+
+  //appendnew_flow_comments_start
+
+  //appendnew_node
+
+  async errorHandler(bh, e, src, parentSpanInst?, functionName?) {
+    console.error(e);
+    bh.error = e;
+    bh.errorSource = src;
+    bh.errorFunName = functionName;
+    this.tracerService.sendData(parentSpanInst, bh, true);
+    if (
+      false
+      /*appendnew_next_Catch*/
+    ) {
+      return bh;
+    } else {
+      if (bh.web.next) {
+        bh.web.next(e);
+      } else {
+        throw e;
+      }
+    }
+  }
+  //appendnew_flow_comments_Catch
+}
